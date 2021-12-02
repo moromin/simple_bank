96629593ad1d5d10b789bdca958658c62a901f48
moromin <34294957+moromin@users.noreply.github.com>
2021-12-02 14:07:39 +0900

    Add: app.env to load config from .env file instead of 'const' that hardcoded in .go files

--- main.go
+++ main.go
@@ -7,16 +7,15 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/moromin/simple_bank/api"
 	db "github.com/moromin/simple_bank/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/moromin/simple_bank/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DbDriver, config.DbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
